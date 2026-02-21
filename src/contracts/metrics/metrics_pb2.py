4e7bb47ac66860fd9dc0e52fb8cb7349bac8ea20
domster704 <domster704@mail.ru>
2026-02-21 22:25:41 +0300

    remove unnecessary fields

--- src/contracts/metrics/metrics_pb2.py
+++ src/contracts/metrics/metrics_pb2.py
@@ -24,7 +24,7 @@ _sym_db = _symbol_database.Default()
 
 
 
-DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x1f\x63ontracts/metrics/metrics.proto\x12\x07metrics\"\x11\n\x03\x41\x63k\x12\n\n\x02ok\x18\x01 \x01(\x08\"\xba\x01\n\x0bNodeMetrics\x12\x0f\n\x07node_id\x18\x01 \x01(\t\x12\x0c\n\x04host\x18\x02 \x01(\t\x12\x0c\n\x04port\x18\x03 \x01(\x05\x12\x19\n\x11timestamp_unix_ms\x18\x04 \x01(\x03\x12\x10\n\x08\x63pu_util\x18\x05 \x01(\x01\x12\x10\n\x08mem_util\x18\x06 \x01(\x01\x12\x14\n\x0cnet_in_bytes\x18\x07 \x01(\x01\x12\x15\n\rnet_out_bytes\x18\x08 \x01(\x01\x12\x12\n\nlatency_ms\x18\t \x01(\x01\x32\x43\n\x0eMetricsService\x12\x31\n\x0bPushMetrics\x12\x14.metrics.NodeMetrics\x1a\x0c.metrics.Ackb\x06proto3')
+DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\x1f\x63ontracts/metrics/metrics.proto\x12\x07metrics\"\x11\n\x03\x41\x63k\x12\n\n\x02ok\x18\x01 \x01(\x08\"}\n\x0bNodeMetrics\x12\x0f\n\x07node_id\x18\x01 \x01(\t\x12\x0c\n\x04port\x18\x02 \x01(\x05\x12\x10\n\x08\x63pu_util\x18\x03 \x01(\x01\x12\x10\n\x08mem_util\x18\x04 \x01(\x01\x12\x14\n\x0cnet_in_bytes\x18\x05 \x01(\x01\x12\x15\n\rnet_out_bytes\x18\x06 \x01(\x01\x32\x43\n\x0eMetricsService\x12\x31\n\x0bPushMetrics\x12\x14.metrics.NodeMetrics\x1a\x0c.metrics.Ackb\x06proto3')
 
 _globals = globals()
 _builder.BuildMessageAndEnumDescriptors(DESCRIPTOR, _globals)
@@ -33,8 +33,8 @@ if not _descriptor._USE_C_DESCRIPTORS:
   DESCRIPTOR._loaded_options = None
   _globals['_ACK']._serialized_start=44
   _globals['_ACK']._serialized_end=61
-  _globals['_NODEMETRICS']._serialized_start=64
-  _globals['_NODEMETRICS']._serialized_end=250
-  _globals['_METRICSSERVICE']._serialized_start=252
-  _globals['_METRICSSERVICE']._serialized_end=319
+  _globals['_NODEMETRICS']._serialized_start=63
+  _globals['_NODEMETRICS']._serialized_end=188
+  _globals['_METRICSSERVICE']._serialized_start=190
+  _globals['_METRICSSERVICE']._serialized_end=257
 # @@protoc_insertion_point(module_scope)
